b13925d5f37bba8f3746e781e51e2e73a195fda2
Dan Thareja <danthareja@gmail.com>
2018-10-23 10:43:37 +0200

    Add error message when prisma is not generated

--- app/connectors/prisma/index.js
+++ app/connectors/prisma/index.js
@@ -1,3 +1,11 @@
-const { prisma } = require('./client');
+try {
+  const { prisma } = require('./generated/client');
+  module.exports = prisma;
+} catch (e) {
+  if (e.code === 'MODULE_NOT_FOUND') {
+    console.log('Please generate the prisma client with `npx run prisma generate`')
+    process.exit(1);
+  }
+  throw e
+}
 
-module.exports = prisma;
