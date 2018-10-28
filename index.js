b92f0f05da6e011c805c946cdff433ac5322a6ad
Dan Thareja <danthareja@gmail.com>
2018-10-28 12:22:56 +0100

    Fix Heroku config conflict

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 
-const app = require('./app');
+const app = require('./app/index.js');
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
-  console.log(`🚀 Server ready at http://localhost:4000`)
+  console.log(`🚀 Server ready at http://localhost:4000`);
   console.log(`🚀 Prisma ready at ${process.env.PRISMA_ENDPOINT}`);
 });
