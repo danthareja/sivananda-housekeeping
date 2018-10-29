9d316d72d71804d37e26ab13408569a2888ab865
Dan Thareja <danthareja@gmail.com>
2018-10-29 07:21:21 +0100

    Add authentication (#6)

---
+++ client/src/routes.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Route, Router, Switch, Redirect } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import App from './App/App';
+import Login from './Login/Login';
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
+
+const history = createHistory();
+const auth = new Auth(history);
+
+export const makeMainRoutes = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <PublicRoute path="/login" component={Login} />
+        <PublicRoute path="/callback" component={Callback} />
+        <PrivateRoute path="/" component={App} />
+      </Switch>
+    </Router>
+  );
+};
+
+const PublicRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={props => <Component auth={auth} {...props} />} />
+  );
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        auth.isAuthenticated() ? (
+          <Component auth={auth} {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
