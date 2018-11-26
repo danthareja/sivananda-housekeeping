34c593203a3eb2b53a510c3fe184cca6baa8721a
Dan Thareja <danthareja@gmail.com>
2018-11-26 12:25:16 -0500

    Filter null rooms

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -3,11 +3,15 @@ const moment = require('moment');
 module.exports = {
   Query: {
     async rooms(root, { date = moment().format('YYYY-MM-DD') }, ctx) {
-      return ctx.db.RoomDay.find({ date })
+      const rooms = await ctx.db.RoomDay.find({ date })
         .cache(0, 'Rooms')
         .populate('room')
         .lean()
         .exec();
+
+      // Temporary fix - find out why room.room is sometimes null (like 2018-11-26)
+      // and ideally add a error message for the client
+      return rooms.filter(room => room.room);
     },
     async room(root, { id, date = moment().format('YYYY-MM-DD') }, ctx) {
       return ctx.db.RoomDay.findOne({ room: id, date })
