f13476a19722595330d7f1be62e58e8684eafb03
Dan Thareja <danthareja@gmail.com>
2018-12-23 16:10:22 -0700

    Add date picker on client

--- client/src/App/components/RoomTable/index.js
+++ client/src/App/components/RoomTable/index.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import moment from 'moment';
 import { Query } from 'react-apollo';
-import { message, Table } from 'antd';
+import { message, Table, DatePicker } from 'antd';
 
 import columns from './columns';
-import title from './title/index.js'; // not sure why index.js is needed here...
 
 import './index.css';
 
 const GET_ROOMS = gql`
-  query GetRooms {
-    rooms {
+  query GetRooms($date: String!) {
+    rooms(date: $date) {
       id
       name
       lodgingName
@@ -49,6 +49,7 @@ const GET_ROOMS = gql`
 class RoomTable extends Component {
   state = {
     roomSearchText: '',
+    date: moment(),
   };
 
   handleRoomSearch = (selectedKeys, confirm) => () => {
@@ -61,9 +62,18 @@ class RoomTable extends Component {
     this.setState({ roomSearchText: '' });
   };
 
+  handleDateChange = date => {
+    this.setState({ date });
+  };
+
   render() {
+    const { date } = this.state;
     return (
-      <Query query={GET_ROOMS} pollInterval={3000}>
+      <Query
+        query={GET_ROOMS}
+        variables={{ date: date.format('YYYY-MM-DD') }}
+        pollInterval={3000}
+      >
         {({ loading, error, data }) => {
           if (error) {
             message.error(error.message);
@@ -71,17 +81,23 @@ class RoomTable extends Component {
           }
 
           return (
-            <Table
-              dataSource={
-                data.rooms && data.rooms.sort((a, b) => a.priority - b.priority)
-              }
-              columns={columns.map(column => column(this))}
-              title={title}
-              loading={loading}
-              pagination={false}
-              rowKey="id"
-              scroll={{ x: 650 }}
-            />
+            <div>
+              <div style={{ marginBottom: '15px' }}>
+                <DatePicker value={date} onChange={this.handleDateChange} />
+              </div>
+
+              <Table
+                dataSource={
+                  data.rooms &&
+                  data.rooms.sort((a, b) => a.priority - b.priority)
+                }
+                columns={columns.map(column => column(this))}
+                loading={loading}
+                pagination={false}
+                rowKey="id"
+                scroll={{ x: 650 }}
+              />
+            </div>
           );
         }}
       </Query>
